2b35ea8413cdff644177b84ad38d3ceb6eb122c1
George <georgi.nanev@ovoenergy.com>
2021-09-22 23:35:11 +0300

    added migrations

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,23 @@
 pragma solidity ^0.8.0;
 
 contract Election {
-    // constructor
-    // store candidate
-    string public candidate;
+    // model a candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+    mapping(uint256 => Candidate) public candidates;
+
+    uint256 public candidatesCount;
 
     // read condidate
     function StartElection() public {
-        candidate = "Candidate 1";
+        // candidate = "Candidate 1";
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
