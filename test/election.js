abfb1d8b1f77bd5e4b7b869110b45235231046ff
George <georgi.nanev@ovoenergy.com>
2021-09-26 22:46:03 +0300

    new tests WIP

--- test/election.js
+++ test/election.js
@@ -1,11 +1,27 @@
-const Election = artifacts.require("Election.sol");
+const Election = artifacts.require("../contracts/Election.sol");
 
 contract("Election", function(accounts) {
     it("initialises with two candidates", function() {
         return Election.deployed().then(function(instance) {
             return instance.candidatesCount();
         }).then(function(count) {
-            assert.equal(count, 2);
+            assert.equal(count.toString(), 2);
         });
     });
+
+    it("it initializes the candidates with the correct values", function() {
+        return Election.deployed().then(function(instance) {
+          electionInstance = instance;
+          return electionInstance.candidates(1);
+        }).then(function(candidate) {
+          assert.equal(candidate[0], 1, "contains the correct id");
+          assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+          assert.equal(candidate[2], 0, "contains the correct votes count");
+          return electionInstance.candidates(2);
+        }).then(function(candidate) {
+          assert.equal(candidate[0], 2, "contains the correct id");
+          assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+          assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+      });
 });
